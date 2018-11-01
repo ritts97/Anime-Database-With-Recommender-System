01d66d20fc20e6a3589707f3218cd080334d555f
Ritwika Ghosh <ritwikaghosh@users.noreply.github.com>
2018-11-02 00:46:39 +0530

    Add files via upload
    Python file for database creation

---
+++ database_creation.py
@@ -0,0 +1,13 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+
+if __name__ == '__main__':
+    create_connection("animedatabase.db")
